381e2ab7ed82273be0daf1096d69fd7c4a70c72e
Dennis Campagna <denniscampagna@gmail.com>
2018-02-03 18:17:24 +0100

    Various changes and improvements
    Days changed to Italian language;
    Monday is now the first day of the week;
    Added possibility to have multiple events on the same day

--- js/demo.js
+++ js/demo.js
@@ -1,8 +1,25 @@
 var events = [
-  {'Date': new Date(2016, 6, 7), 'Title': 'Doctor appointment at 3:25pm.'},
-  {'Date': new Date(2016, 6, 18), 'Title': 'New Garfield movie comes out!', 'Link': 'https://garfield.com'},
-  {'Date': new Date(2016, 6, 27), 'Title': '25 year anniversary', 'Link': 'https://www.google.com.au/#q=anniversary+gifts'},
+    {
+        'Date': new Date(2018, 1, 7), 
+        'Title': ['Evento1', 'Evento2', 'Evento3'], 
+        Link: [
+            '#linkEvento1', 
+            '#linkEvento2',
+            '#linkEvento3'
+        ]
+    },
+    {
+        'Date': new Date(2018, 1, 14), 
+        'Title': ['Evento1', 'Evento2'], 
+        Link: [
+            '#linkEvento1', 
+            function() {
+                alert("Click on Evento2!");
+            },
+        ]
+    }
 ];
+
 var settings = {};
 var element = document.getElementById('caleandar');
 caleandar(element, events, settings);
